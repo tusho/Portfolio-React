592a22a362485c76dd51a771459309cf36c30773
Tusho <armin.kuhestani@gmail.com>
2018-09-05 13:29:00 +1000

    added header and hero image

--- src/App.js
+++ src/App.js
@@ -4,11 +4,23 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="header">
-        <div className="header_inner">
-          <img className="header_logo" src={ require("./img/logo.png") }/>
+      <div className="app">
+        <div className="header">
+          <div className="header_inner">
+            <img className="header_logo" src={ require("./img/logo.png") }/>
+            <section className="header_title">
+              <h1 className="title_name">Armin Kuhestani</h1>
+              <h2 className="title_pitch">Carefully Crafted Code</h2>
+            </section>
+          </div>
+        </div>
+        <hr />
+        <div className="hero">
+          <img className="hero_image_small" src={ require("./img/desksmall.jpg") }/>
+          <img className="hero_image_large" src={ require("./img/desklarge.jpg") }/>
         </div>
       </div>
+
     );
   }
 }
