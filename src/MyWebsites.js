c4c8524a0cfe481582d859926d19b8063f9c8802
Tusho <armin.kuhestani@gmail.com>
2018-09-11 14:30:38 +1000

    changed margin and added image files

--- src/MyWebsites.js
+++ src/MyWebsites.js
@@ -1,21 +1,30 @@
 import React, { Component } from 'react'
 import sortBy from 'sort-by'
 
-const websiteData = [
-    { websiteURL: 'https://github.com/tusho', image: 'alift', name: 'A-Lift' },
-    { websiteURL: 'https://hackerfirm.com', image: 'hackerfirm', name: 'Hacker Firm' },
-    { websiteURL: 'https://json.capital', image: 'json', name: 'JSON Capital' }
-];
+
+
+
 
 class MyWebsites extends Component {
+
   render() {
+    const websiteData = [
+        { websiteURL: 'https://integratednaturalsolutions.com', image: 'integratednaturalsolutions', name: 'INS Global' },
+        { websiteURL: 'https://json.capital', image: 'json', name: 'JSON Capital' },
+        { websiteURL: 'https://inatureskincare.com', image: 'inatureskincare', name: 'iNature Skincare' },
+        { websiteURL: 'https://lucaspapawdistributor.com', image: 'lucaspapaw', name: 'Lucas Papaw' }
+
+    ];
+
+    websiteData.sort(sortBy('website.name'))
+
     return (
       <div className="work">
-        <h2 class="work_header">My Websites</h2>
+        <h2 className="work_header">My Websites</h2>
         {websiteData.map(website => (
           <section className="projects">
             <a href={website.websiteURL} target="_blank">
-              <img src={require(`./img/${website.image}.jpg`)} className="project_image" />
+              <img src={require(`./img/${website.image}.jpg`)} className="project_image"/>
             </a>
             <h3 className="project_name">{website.name}</h3>
             <a className="project_links" href={website.websiteURL} target="_blank">{website.websiteURL}</a>
