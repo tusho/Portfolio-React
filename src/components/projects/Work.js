7d0e757ab2ff6c5408e4dc3caf2e8314fbe876ed
Armin Kuhestani <armin_kuhestani@apple.com>
2019-02-26 15:23:53 +1100

    fixed grid layout

--- src/components/projects/Work.js
+++ src/components/projects/Work.js
@@ -30,21 +30,27 @@ class Work extends Component {
 
     const appData = [
         { websiteURL: 'https://tusho.github.io/Memory-Game/index.html', image: 'memorygame', name: 'Memory Game', type: 'app', stack: ['JavaScript', 'HTML', 'CSS'] },
-        { websiteURL: 'https://tusho.github.io/Arcade-Game/index.html', image: 'arcadegame', name: 'Frogger Arcade Game', type: 'app', stack: ['JavaScript', 'HTML', 'CSS'] }
+        { websiteURL: 'https://tusho.github.io/Arcade-Game/index.html', image: 'arcadegame', name: 'Frogger Arcade Game', type: 'app', stack: ['JavaScript', 'HTML', 'CSS'] },
+        { websiteURL: 'https://bookshelf-app-tusho.herokuapp.com', image: 'bookshelf-app', name: 'Bookshelf App', type: 'app', stack: ['React', 'JavaScript', 'HTML', 'CSS'] },
+        { websiteURL: 'https://sydney-venue-map.herokuapp.com', image: 'sydney-venue-map', name: 'Sydney Venues', type: 'app', stack: ['React', 'JavaScript', 'HTML', 'CSS'] }
     ]
 
     websiteData.sort(sortBy('name'))
 
     return (
         <div className="work">
-            <h2 className="work_header">My Websites<hr className="project_divider"/></h2>
-            {websiteData.map(website => (
-                this.state.isHovering == false ? <Projects website={website} mouseHover={this.handleMouseHover}/> : <div><p>{website.stack}</p></div>
-            ))}
-            <h2 className="work_header">My Apps<hr className="project_divider"/></h2>
-            {appData.map(website => (
-                this.state.isHovering == false ? <Projects website={website} mouseHover={this.handleMouseHover}/> : <div><p>{website.stack}</p></div>
-            ))}
+            <div class="work_container websites">
+                <h2 className="work_header">My Websites<hr className="project_divider"/></h2>
+                {websiteData.map(website => (
+                    this.state.isHovering == false ? <Projects website={website} mouseHover={this.handleMouseHover}/> : <div><p>{website.stack}</p></div>
+                ))}
+            </div>
+            <div class="work_container apps">
+                <h2 className="work_header">My Apps<hr className="project_divider"/></h2>
+                {appData.map(website => (
+                    this.state.isHovering == false ? <Projects website={website} mouseHover={this.handleMouseHover}/> : <div><p>{website.stack}</p></div>
+                ))}
+            </div>
         </div>
     )
   }
